87c619641d7c7156f04560ead3406517e368bfc4
alvin <alvin@revix.com>
2022-11-15 13:05:48 +0200

    add data model and entity

---
+++ UserDirectory.Domain/Models/Dtos/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace UserDirectory.Domain.Models.Dtos
+{
+    public class CustomerDto
+    {
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
+        public string? date_of_birth { get; set; }
+        public string? email_address { get; set; }
+        public string? address { get; set; }
+        public string? city { get; set; }
+        public string? country { get; set; }
+        public string? zip_code { get; set; }
+    }
+}
---
+++ UserDirectory.Domain/Models/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace UserDirectory.Domain.Models.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? ZipCode { get; set; }
+
+    }
+}
